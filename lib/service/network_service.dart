76fd706bb03988b8033f75e64f86098793e758fb
Ryan White <ryan.thomas.white@outlook.com>
2020-02-01 17:10:51 -0500

    Updated endpoint.

--- lib/service/network_service.dart
+++ lib/service/network_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class NetworkService {
   static Future<String> getQuizItemsForAccount() async {
-    var url = 'https://studyq-backend-6cmwkb4jva-ue.a.run.app/studyQ/mattest';
+    var url = 'https://studyq-backend-6cmwkb4jva-ue.a.run.app/studyQ/datastore_fetch';
     var response = await http.get(url);
     log('Response status: ${response.statusCode}');
     log('Response body: ${response.body}');
